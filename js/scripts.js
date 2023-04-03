6f2207742fa261eb9095a7c1d5efc05376362c6f
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-04-03 12:29:53 +0200

    added pokemon list for task 2

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+const pokemonList = [];
+
+pokemonList[0] = {
+    'name': 'Bulbasaur',
+    'height': 0.7,
+    'types': ['grass', 'poison']
+};
+
+pokemonList[1] = {
+    'name': 'Ivysaur',
+    'height': 1.0,
+    'types': ['grass', 'poison']
+};
+
+pokemonList[2] = {
+    'name': 'Venusaur',
+    'height': 2.0,
+    'types': ['grass', 'poison']
+};
