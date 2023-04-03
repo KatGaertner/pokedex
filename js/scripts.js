9dc0fd509cbc16906c537dc50e8ff25ba0f1008e
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-04-03 13:35:22 +0200

    switched to defining the list directly

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,22 @@
-const pokemonList = [];
+const pokemonList = [
+    {
+        'name': 'Bulbasaur',
+        'height': 0.7,
+        'types': ['grass', 'poison']
+    },
+    {
+        'name': 'Ivysaur',
+        'height': 1.0,
+        'types': ['grass', 'poison']
+    },
+    {
+        'name': 'Venusaur',
+        'height': 2.0,
+        'types': ['grass', 'poison']
+    }
+];
+
 const noteText = ' - Wow, that\'s big!'
-
-
-pokemonList[0] = {
-    'name': 'Bulbasaur',
-    'height': 0.7,
-    'types': ['grass', 'poison']
-};
-
-pokemonList[1] = {
-    'name': 'Ivysaur',
-    'height': 1.0,
-    'types': ['grass', 'poison']
-};
-
-pokemonList[2] = {
-    'name': 'Venusaur',
-    'height': 2.0,
-    'types': ['grass', 'poison']
-};
-
 
 for (let i = 0; i < pokemonList.length; i++) {
     let note = '';
