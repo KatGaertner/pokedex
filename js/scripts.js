6d9ee6744eb8f90577811c12e257013ab794f04d
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-04-06 19:24:52 +0200

    removed unused code

--- js/scripts.js
+++ js/scripts.js
@@ -56,17 +56,7 @@ const pokemonRepo = (function () {
     };
 })();
 
-
-function checkHeight(pokemon) {
-    if (pokemon.height > 1.0) {
-        return ' - Wow, that\'s big!';
-    } else {
-        return '';
-    }
-}
-
 function displayPokemon(pokemon) {
-    let note = checkHeight(pokemon);
     document.write(`${pokemon.name}, height: ${pokemon.height}, type: ${pokemon.types} <br>`);
 }
 
