2d81f598a29f19eb76f81b7b476c4ef26c8adc0c
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-04-06 14:55:56 +0200

    changed format of list keys

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 const pokemonList = [
     {
-        'name': 'Bulbasaur',
-        'height': 0.7,
-        'types': ['grass', 'poison']
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['grass', 'poison']
     },
     {
-        'name': 'Ivysaur',
-        'height': 1.0,
-        'types': ['grass', 'poison']
+        name: 'Ivysaur',
+        height: 1.0,
+        types: ['grass', 'poison']
     },
     {
-        'name': 'Venusaur',
-        'height': 2.0,
-        'types': ['grass', 'poison']
+        name: 'Venusaur',
+        height: 2.0,
+        types: ['grass', 'poison']
     }
 ];
 
